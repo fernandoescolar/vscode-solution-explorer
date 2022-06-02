c2bdb6f959b2eeb91da7111fa01f5a3cd1cc64fa
Fernando Escolar <fernando.escolar@scrm.lidl>
2022-06-02 17:35:05 +0200

    #171 getting project templates from "dotnet new --list" command output

--- src/commands/AddNewProjectCommand.ts
+++ src/commands/AddNewProjectCommand.ts
@@ -1,4 +1,5 @@
 import * as path from "path";
+import { execSync } from 'child_process';
 import { CliCommandBase } from "./base/CliCommandBase";
 import { SolutionExplorerProvider } from "../SolutionExplorerProvider";
 import { TreeItem } from "../tree/TreeItem";
@@ -7,29 +8,29 @@ import { InputTextCommandParameter } from "./parameters/InputTextCommandParamete
 import { InputOptionsCommandParameter } from "./parameters/InputOptionsCommandParameter";
 
 const ProjectTypes = [
-    { name: 'Console application', value: 'console', languages: ['C#', 'F#', 'VB'] },
-    { name: 'Class library', value: 'classlib', languages: ['C#', 'F#', 'VB'] },
-    { name: 'WPF Application', value: 'wpf', languages: ['C#'] },
-    { name: 'WPF Class library', value: 'wpflib', languages: ['C#'] },
-    { name: 'WPF Custom Control Library', value: 'wpfcustomcontrollib', languages: ['C#'] },
-    { name: 'WPF User Control Library', value: 'wpfusercontrollib', languages: ['C#'] },
-    { name: 'Windows Forms (WinForms) Application', value: 'winforms', languages: ['C#'] },
-    { name: 'Windows Forms (WinForms) Class library', value: 'winformslib', languages: ['C#'] },
-    { name: 'Worker Service', value: 'worker', languages: ['C#'] },
-    { name: 'Unit test project', value: 'mstest', languages: ['C#', 'F#', 'VB'] },
-    { name: 'xUnit test project', value: 'xunit', languages: ['C#', 'F#', 'VB'] },
-    { name: 'NUnit 3 Test Project', value: 'nunit', languages: ['C#', 'F#', 'VB'] },
-    { name: 'ASP.NET Core empty', value: 'web', languages: ['C#', 'F#'] },
-    { name: 'ASP.NET Core Web App (Model-View-Controller)', value: 'mvc', languages: ['C#', 'F#'] },
-    { name: 'ASP.NET Core Web App', value: 'razor', languages: ['C#'] },
-    { name: 'ASP.NET Core with Angular', value: 'angular', languages: ['C#'] },
-    { name: 'ASP.NET Core with React.js', value: 'react', languages: ['C#'] },
-    { name: 'ASP.NET Core with React.js and Redux', value: 'reactredux', languages: ['C#'] },
-    { name: 'ASP.NET Core Web API', value: 'webapi', languages: ['C#', 'F#'] },
-    { name: 'ASP.NET Core gRPC Service', value: 'grpc', languages: ['C#'] },
-    { name: 'Blazor Server App', value: 'blazorserver', languages: ['C#'] },
-    { name: 'Blazor WebAssembly App', value: 'blazorwasm', languages: ['C#'] },
-    { name: 'Razor Class Library', value: 'razorclasslib', languages: ['C#'] },
+    // { name: 'Console application', value: 'console', languages: ['C#', 'F#', 'VB'] },
+    // { name: 'Class library', value: 'classlib', languages: ['C#', 'F#', 'VB'] },
+    // { name: 'WPF Application', value: 'wpf', languages: ['C#'] },
+    // { name: 'WPF Class library', value: 'wpflib', languages: ['C#'] },
+    // { name: 'WPF Custom Control Library', value: 'wpfcustomcontrollib', languages: ['C#'] },
+    // { name: 'WPF User Control Library', value: 'wpfusercontrollib', languages: ['C#'] },
+    // { name: 'Windows Forms (WinForms) Application', value: 'winforms', languages: ['C#'] },
+    // { name: 'Windows Forms (WinForms) Class library', value: 'winformslib', languages: ['C#'] },
+    // { name: 'Worker Service', value: 'worker', languages: ['C#'] },
+    // { name: 'Unit test project', value: 'mstest', languages: ['C#', 'F#', 'VB'] },
+    // { name: 'xUnit test project', value: 'xunit', languages: ['C#', 'F#', 'VB'] },
+    // { name: 'NUnit 3 Test Project', value: 'nunit', languages: ['C#', 'F#', 'VB'] },
+    // { name: 'ASP.NET Core empty', value: 'web', languages: ['C#', 'F#'] },
+    // { name: 'ASP.NET Core Web App (Model-View-Controller)', value: 'mvc', languages: ['C#', 'F#'] },
+    // { name: 'ASP.NET Core Web App', value: 'razor', languages: ['C#'] },
+    // { name: 'ASP.NET Core with Angular', value: 'angular', languages: ['C#'] },
+    // { name: 'ASP.NET Core with React.js', value: 'react', languages: ['C#'] },
+    // { name: 'ASP.NET Core with React.js and Redux', value: 'reactredux', languages: ['C#'] },
+    // { name: 'ASP.NET Core Web API', value: 'webapi', languages: ['C#', 'F#'] },
+    // { name: 'ASP.NET Core gRPC Service', value: 'grpc', languages: ['C#'] },
+    // { name: 'Blazor Server App', value: 'blazorserver', languages: ['C#'] },
+    // { name: 'Blazor WebAssembly App', value: 'blazorwasm', languages: ['C#'] },
+    // { name: 'Razor Class Library', value: 'razorclasslib', languages: ['C#'] },
 ];
 
 export class AddNewProjectCommand extends CliCommandBase {
@@ -43,6 +44,7 @@ export class AddNewProjectCommand extends CliCommandBase {
     }
 
     protected shouldRun(item: TreeItem): boolean {
+        this.loadProjectTemplates();
         this.parameters = [
             new StaticCommandParameter('new'),
             new InputOptionsCommandParameter('Select project type', this.getProjectTypes()),
@@ -52,6 +54,30 @@ export class AddNewProjectCommand extends CliCommandBase {
         ];
 
         return true;
+    }
+
+    private loadProjectTemplates(): void {
+        if (ProjectTypes.length > 0) return;
+
+        let buffer = execSync('dotnet new --list');
+        if (!buffer) {
+            return;
+        }
+
+        let lines = buffer.toString().split('\n');
+        if (lines.length > 4) {
+            lines.splice(0, 4); /* ignore header */
+            lines.forEach(line => {
+                let parts = line.split('  ').filter(element => element);
+                if (parts.length > 2) {
+                    ProjectTypes.push({
+                        name: parts[0].trim(),
+                        value: parts[1].trim(),
+                        languages: parts[2].split(',').map(element => element.trim().replace('[', '').replace(']', ''))
+                    });
+                }
+            });
+        }
     }
 
     private getProjectTypes(): { [id:string]: string } {
