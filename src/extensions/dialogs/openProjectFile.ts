ae8cbc3836313f70be7603fc83218447302c96f1
Fernando Escolar <fernando.escolar@scrm.lidl>
2022-09-02 18:22:07 +0200

    #191 adding vcxproj as a known extension in open project dialog

--- src/extensions/dialogs/openProjectFile.ts
+++ src/extensions/dialogs/openProjectFile.ts
@@ -5,7 +5,7 @@ export async function openProjectFile(label: string): Promise<string | undefined
         openLabel: label,
         canSelectFolders: false,
         canSelectMany: false,
-        filters: { ['Projects']: [ 'csproj', 'vbproj', 'fsproj' ] }
+        filters: { ['Projects']: [ 'csproj', 'vbproj', 'fsproj', 'vcxproj' ] }
     });
 
     if (uris !== undefined && uris.length > 0) {
