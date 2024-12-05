71e9d5eb4d5e90977e633a050428488c6379334e
Guillermo Pérez <guillermo.perez.ivo@gmail.com>
2024-12-05 09:36:10 +0100

    fix: Included on Directory.Packages.props all PackageReferences on Directory.Build.props files

--- src/actions/CreateDirectoryPackages.ts
+++ src/actions/CreateDirectoryPackages.ts
@@ -1,18 +1,31 @@
 import { Solution } from "@core/Solutions";
 import { Action, ActionContext } from "./base/Action";
 import { DirectoryPackages } from "@core/DirectoryPackages";
+import * as vscode from "vscode";
 
 export class CreateDirectoryPackages implements Action {
-  constructor(private readonly solution: Solution) { }
+  constructor(private readonly solution: Solution) {}
 
   public toString(): string {
     return `Create Directory.Packages.props in solution ${this.solution.name}`;
   }
 
+  private async getDirectoryBuildFiles(): Promise<string[]> {
+    return vscode.workspace
+      .findFiles("**/Directory.Build.props")
+      .then((directoryFiles) => {
+        if (directoryFiles.length == 0) return [];
+        return directoryFiles.map((file) => file.path);
+      });
+  }
   public async execute(context: ActionContext): Promise<void> {
     const projects = this.solution.getAllProjects();
+    const directoryBuildsFiles = await this.getDirectoryBuildFiles();
     const directoryPackage = new DirectoryPackages(this.solution.folderPath);
     await directoryPackage.load();
-    directoryPackage.addProjects(projects);
+    directoryBuildsFiles.forEach(async (f) => {
+      await directoryPackage.addProjectFile(f);
+    });
+    await directoryPackage.addProjects(projects);
   }
 }
